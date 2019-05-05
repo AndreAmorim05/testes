b10bae61f43e9ba8d449d7d20cee34923d826436
André Amorim <js.andreamorim@gmail.com>
2019-05-04 22:22:27 -0300

    Finalizacao da mecanica da tabela interagindo com os widgets de texto

--- tabelas kv/pandas_gridlayout.py
+++ tabelas kv/pandas_gridlayout.py
@@ -1,5 +1,8 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from tabela import Tabela
 
@@ -7,20 +10,16 @@ import pandas as pd
 import numpy as np
 
 
-# dados_Ke = np.array([50,1.0, 90,0.897, 95,0.868, 99,0.814,
-#                     99.9,0.753, 99.99,0.702, 99.999,0.659,
-#                     99.9999,0.620]).reshape(8,2)
-# df = pd.DataFrame(dados_Ke, columns='Confiabilidade (%),Fator de confiabilidade Ke'.split(','))
 
-# print(df.shape)
-df = pd.read_csv('chavetas_paralelas.csv')
+valores = np.array([1.58,-0.085,4.51,-0.265,57.7,-0.0718,272,-0.995]).reshape(4,2)
+df = pd.DataFrame(valores, columns='a b'.split(),
+                    index='Retificado Usinado/Laminado_a_frio Laminado_a_quente Forjado'.split())
 
-# valores = np.array([1.58,-0.085,4.51,-0.265,57.7,-0.0718,272,-0.995]).reshape(4,2)
-# df = pd.DataFrame(valores, columns='a b'.split(),
-#                     index='Retificado Usinado/Laminado_a_frio Laminado_a_quente Forjado'.split())
-# display(df)
 
 class Manager(ScreenManager):
+    pass
+
+class Menu(Screen):
     pass
 
 class Inicio(Screen):
@@ -28,7 +27,12 @@ class Inicio(Screen):
 class Tela1(Screen):
     def __init__(self, **kwargs):
         super(Tela1, self).__init__(**kwargs)
-        self.add_widget(Tabela(df))
+        self.pop = None #inicialização importante para usar o dismiss() no TextInput
+
+    def req_tabela(self, *args):
+        ids = [self.ids.tex1, self.ids.tex2]
+        self.pop = Popup(title='Tabela', content=Tabela(DataFrame=df, ids_list=ids), size_hint=(0.5, 0.5))
+        self.pop.open()
 
 class PD_Grid(App):
     def build(self):
@@ -36,5 +40,3 @@ class PD_Grid(App):
 
 if __name__ == '__main__':
     PD_Grid().run()
-
-# tab.clear_widgets()
--- tabelas kv/tabela.py
+++ tabelas kv/tabela.py
@@ -2,15 +2,18 @@ from kivy.uix.button import ButtonBehavior
 from kivy.uix.label import Label
 from kivy.graphics import Color, Rectangle
 from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty, ListProperty
 from kivy.clock import Clock
 from kivy.metrics import dp
 
 
 
 class Tabela(GridLayout):
-    def __init__(self, DataFrame, **kwargs):
+    DataFrame = ObjectProperty()
+    ids_list = ListProperty()
+    def __init__(self, **kwargs):
         super(Tabela, self).__init__(**kwargs)
-        self.data = DataFrame
+        self.data = self.DataFrame
         self.cols = self.tamanho()
         self.spacing = dp(2)
         self.colunas = list(self.data.columns.values)
@@ -35,11 +38,23 @@ class Tabela(GridLayout):
                 self.add_widget(Label(text=str(row[col])))
     
     def pegar_dados(self, index, *args):
+        '''
+        Nesta função é recebido o índice escolhido ao se clicar na tabela,
+        o valor recebido por index é o indice, este indice é buscado no
+        DataFrame recebido e adiciona o valor de cada coluna da linha do 
+        índice ao seu respectivo Label ou TextInput, desde que os ids destes
+        tenham sido passados pela variável ids_list, os ids passados devem
+        ser da forma:
+        >>> [self.ids.nome_do_id_do_widget_1, self.ids.nome_do_id_do_widget_2...]
+        A quantidade de ids passados deve ser menor ou igual a quantidade de colunas
+        do DataFrame, caso seja maior, alguns widgets irão ficar sem texto.
+        '''
         try:
-            print(self.data.loc[index].tolist())
+            for ids, data in zip(self.ids_list, self.data.loc[index].tolist()):
+                ids.text = str(data)
         except:
-            print(self.data.loc[float(index)].tolist())
-        # return self.data.loc[int(index)].tolist()
+            for ids, data in zip(self.ids_list, self.data.loc[float(index)].tolist()):
+                ids.text = str(data)
 
 class ButtonStyle(ButtonBehavior, Label):
     def __init__(self, tex, ref, **kwargs):
